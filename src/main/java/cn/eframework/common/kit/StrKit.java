87f8265436008cf73278dc8414f7c7611bd849d7
张良 <zhangliang0115@126.com>
2016-11-10 14:19:02 +0800

    测试提交

--- src/main/java/cn/eframework/common/kit/StrKit.java
+++ src/main/java/cn/eframework/common/kit/StrKit.java
@@ -4,7 +4,7 @@ package cn.eframework.common.kit;
  * Created by zhangliang on 2016/11/10.
  */
 public class StrKit {
-    public static String cs(String a){
+    public static void cs(String a){
         System.out.print("csss");
     }
 }
