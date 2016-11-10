2afc110cb5bdef86b741ae0db2c1e75a86c1af70
张良 <zhangliang0115@126.com>
2016-11-10 14:18:37 +0800

    测试提交

---
+++ src/main/java/cn/eframework/common/kit/StrKit.java
@@ -0,0 +1,10 @@
+package cn.eframework.common.kit;
+
+/**
+ * Created by zhangliang on 2016/11/10.
+ */
+public class StrKit {
+    public static String cs(String a){
+        System.out.print("csss");
+    }
+}
